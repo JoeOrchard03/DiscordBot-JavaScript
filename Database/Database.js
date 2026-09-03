a639b312b5121d276b0b86e45d404e6a32fcd913
Joe Orchard <joe.orchard03@gmail.com>
2026-09-03 13:53:23 +0000

    Added code to create a sql database and write and read data to/from it

--- Database/Database.js
+++ Database/Database.js
@@ -2,4 +2,27 @@ const { DatabaseSync } = require("node:sqlite");
 
 const database = new DatabaseSync("Database/artificer.db"); //Creates/opens database
 
+//Runs SQL against the database and creates table if one does not already exist
+database.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+        discord_user_id TEXT PRIMARY KEY,
+        tarkov_name TEXT)`);
+
+//prepare is used to change table values, INSERT INTO is used to write data
+const insertUser = database.prepare(`
+    INSERT INTO users (discord_user_id, tarkov_name)
+    VALUES (?,?)`); //?s are placeholder values
+
+//SELECT is used to read data
+const getUser = database.prepare(`
+    SELECT *
+    FROM users
+    WHERE discord_user_id = ?`);
+
+insertUser.run("12345", "Joe");
+    
+const user = getUser.get("12345");
+
+console.log(user);
+
 console.log("Database connected");

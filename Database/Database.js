6141acf10a4feb5aba6cb49cdc675e423a7ff5c8
Joe Orchard <joe.orchard03@gmail.com>
2026-09-03 14:05:52 +0000

    Refactored Database.js to be more friendly

--- Database/Database.js
+++ Database/Database.js
@@ -2,27 +2,39 @@ const { DatabaseSync } = require("node:sqlite");
 
 const database = new DatabaseSync("Database/artificer.db"); //Creates/opens database
 
+function addUser(discordUserID, tarkovName)
+{
+    //INSERT INTO is used to write data, ignores if that user already exists
+    const insertUser = database.prepare(`
+        INSERT OR IGNORE INTO users (discord_user_id, tarkov_name)
+        VALUES (?,?)`); //?s are placeholder values
+
+    insertUser.run(discordUserID, tarkovName);
+}
+
+function getUser(discordUserID)
+{
+    //SELECT is used to read data
+    const getUser = database.prepare(`
+        SELECT *
+        FROM users
+        WHERE discord_user_id = ?`);
+
+    return getUser.get(discordUserID);
+}
+
 //Runs SQL against the database and creates table if one does not already exist
 database.exec(`
     CREATE TABLE IF NOT EXISTS users (
         discord_user_id TEXT PRIMARY KEY,
         tarkov_name TEXT)`);
-
-//prepare is used to change table values, INSERT INTO is used to write data
-const insertUser = database.prepare(`
-    INSERT INTO users (discord_user_id, tarkov_name)
-    VALUES (?,?)`); //?s are placeholder values
-
-//SELECT is used to read data
-const getUser = database.prepare(`
-    SELECT *
-    FROM users
-    WHERE discord_user_id = ?`);
-
-insertUser.run("12345", "Joe");
     
-const user = getUser.get("12345");
+addUser("12345", "Joe");
+
+const user = getUser("12345");
 
 console.log(user);
 
 console.log("Database connected");
+
+module.exports = {addUser, getUser};
